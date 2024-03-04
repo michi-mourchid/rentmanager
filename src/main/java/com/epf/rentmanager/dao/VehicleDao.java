729398ba6b5eaf95acb2e65e20e5a3b50d857220
michibou <mourchid.moutuidine@hotmail.com>
2024-03-04 21:06:29 +0100

    interface reservation wip 1

--- src/main/java/com/epf/rentmanager/dao/VehicleDao.java
+++ src/main/java/com/epf/rentmanager/dao/VehicleDao.java
@@ -23,7 +23,7 @@ public class VehicleDao {
 	private static final String DELETE_VEHICLE_QUERY = "DELETE FROM Vehicle WHERE id=?;";
 	private static final String FIND_VEHICLE_QUERY = "SELECT id, constructeur, modele, nb_places FROM Vehicle WHERE id=?;";
 	private static final String FIND_VEHICLES_QUERY = "SELECT id, constructeur, modele, nb_places FROM Vehicle;";
-	
+	 
 	public long create(Vehicle vehicle) throws DaoException {
 		try{
 			int id = 0;
